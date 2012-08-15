d983451e0f38893effe2192fb8b6cae41708e548
Julien Bloit <julien.bloit@gmail.com>
2012-08-15 14:33:21 -0700

    added flare project

---
+++ myProjects/flares/flares/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  flares
+//
+//  Created by Julien Bloit on 15/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
